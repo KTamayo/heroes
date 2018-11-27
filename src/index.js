65806f02e9f894c8428eccb4a644f5ba16107f7a
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-11-26 21:23:58 -0800

    renames redux store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import store from './store/store';
 
 import App from './App';
 
@@ -10,9 +10,9 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={configureStore}>
+  <Provider store={store}>
     <App />
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')  
 );
 
