5606e39f7f0e83db596112d8b1190af3fc195fa9
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-11-26 22:53:37 -0800

    removes unnecessary file imports, comments and image jsx

--- src/App.js
+++ src/App.js
@@ -2,17 +2,13 @@ import React, { Component } from 'react';
 import AppBar from './components/AppBar/AppBar';
 import SearchResults from './components/SearchResults/SearchResults';
 
-import './App.css';
-// import logo from './logo.svg';
-
 class App extends Component {
-  
+
   render() {
     return (
       <div>
         <AppBar />
-        <SearchResults />    
-        {/* <img src={logo} alt='logo' className='App-logo' />*/}
+        <SearchResults />
       </div>
     );
   }
