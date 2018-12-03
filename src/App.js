0adb5c3cbf15153a0cf211182852ffe3c5c27d22
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-12-02 21:08:16 -0800

    refactors for routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
 import AppBar from './components/AppBar/AppBar';
 import SearchResults from './components/SearchResults/SearchResults';
+
+import Route from 'react-router-dom/Route';
+
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <AppBar />
-        <SearchResults />
-      </div>
+      <Fragment>
+        <AppBar />        
+        <Route exact path='/' component={SearchResults} />        
+      </Fragment>      
     );
   }
 }
 
 export default App;
+ 
