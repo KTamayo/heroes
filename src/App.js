3a3c887e9b2f112873e36ce3e0b4bb6ad550efe9
Karlo Tamayo <karlo.tamayo.1010@gmail.com>
2018-12-05 01:08:07 -0800

    adds route handling for hero detail view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 
 import AppBar from './components/AppBar/AppBar';
 import SearchResults from './components/SearchResults/SearchResults';
+import HeroDetail from './components/HeroDetail/HeroDetail';
 
 import Route from 'react-router-dom/Route';
 
@@ -11,8 +12,9 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <AppBar />        
-        <Route exact path='/' component={SearchResults} />        
+        <AppBar />
+        <Route exact path='/' component={SearchResults} />
+        <Route path='/hero_detail' component={HeroDetail} />
       </Fragment>      
     );
   }
